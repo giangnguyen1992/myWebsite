8e5a094a170a9a96bf45e662ce871b311a1ffc7e
Giang Nguyen <tgn1992@hotmail.de>
2018-12-04 23:49:50 +0100

    Updates

---
+++ precache-manifest.e985c39206637805d82c80351549e9c4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "52066bd48dcf105988caffd3c3a9c688",
+    "url": "/giangnguyen/static/media/hplogo.52066bd4.png"
+  },
+  {
+    "revision": "8bec41a89639c75528e7",
+    "url": "/giangnguyen/static/css/main.e239cbce.chunk.css"
+  },
+  {
+    "revision": "41737ae0e1f7c51f7f7a",
+    "url": "/giangnguyen/static/js/1.41737ae0.chunk.js"
+  },
+  {
+    "revision": "4c4893a95c8b0713d63d",
+    "url": "/giangnguyen/static/js/runtime~main.4c4893a9.js"
+  },
+  {
+    "revision": "5d94526ba295f9d44af3816d020ff198",
+    "url": "/giangnguyen/static/media/3d9ec9a9-76a2-4cfe-b376-76e7340c3b50.5d94526b.eot"
+  },
+  {
+    "revision": "12dae99343ba912bda027ae197549fba",
+    "url": "/giangnguyen/static/media/e0d6f852-5401-4bbf-9672-47a50c5c87c6.12dae993.woff2"
+  },
+  {
+    "revision": "9a62fff53de077b844bc6f86e55cf297",
+    "url": "/giangnguyen/static/media/7fedd582-6ae3-4850-be2f-4acae2e74fa5.9a62fff5.woff"
+  },
+  {
+    "revision": "cf034e78f4e2cacc973cc8ec986a33d9",
+    "url": "/giangnguyen/static/media/d6e08ef3-40db-4ac3-82df-f062f55a72f5.cf034e78.ttf"
+  },
+  {
+    "revision": "8bec41a89639c75528e7",
+    "url": "/giangnguyen/static/js/main.8bec41a8.chunk.js"
+  },
+  {
+    "revision": "ad55d76e643932bf33a2c77d890295de",
+    "url": "/giangnguyen/static/media/Me.ad55d76e.jpg"
+  },
+  {
+    "revision": "87d994875c22d91270c9d27302a45fb7",
+    "url": "/giangnguyen/static/media/Bootstrap.87d99487.png"
+  },
+  {
+    "revision": "80ca1fcae69d1d8272a7f1594323d1a7",
+    "url": "/giangnguyen/static/media/Gulp.80ca1fca.png"
+  },
+  {
+    "revision": "8d859602e4edf37f8e6fbce188479840",
+    "url": "/giangnguyen/static/media/React.8d859602.png"
+  },
+  {
+    "revision": "c13c393f310c02a422138de8d2ba4f31",
+    "url": "/giangnguyen/static/media/SASS.c13c393f.png"
+  },
+  {
+    "revision": "6ded14c8a9e078f1d9c305186271078d",
+    "url": "/giangnguyen/static/media/WordPress.6ded14c8.png"
+  },
+  {
+    "revision": "4de645e16e2d3f5b51cfbe1323f13c9b",
+    "url": "/giangnguyen/static/media/webpack.4de645e1.png"
+  },
+  {
+    "revision": "66bfa6d386371771745c3d4fad9db0b3",
+    "url": "/giangnguyen/index.html"
+  }
+];
